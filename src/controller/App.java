39e6a5afd16773b40a72722b3e8c4cb24a585936
shuya_hu <hushu@sheridancollege.ca>
2023-08-10 23:56:25 -0400

    Finish login page style

--- src/controller/App.java
+++ src/controller/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -38,8 +39,10 @@ public class App extends Application {
     public static void showLoginScreen() {
         try {
             Parent root = FXMLLoader.load(App.class.getResource("../fxml/LoginScreen.fxml"));
-            primaryStage.setTitle("Login Screen");
-            primaryStage.setScene(new Scene(root, 600, 400));
+            App.primaryStage.setTitle("Completed");
+            App.primaryStage.getIcons().add(new Image(App.class.getResourceAsStream("../asset/img/icon.png")));
+            Scene scene = new Scene(root);
+            App.primaryStage.setScene(scene);
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
@@ -49,8 +52,8 @@ public class App extends Application {
     public static void setRoot(String fxmlFile) {
         try {
             Parent root = FXMLLoader.load(App.class.getResource(fxmlFile + ".fxml"));
-            primaryStage.setScene(new Scene(root, 500, 400));
-            primaryStage.setTitle(fxmlFile);
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
         } catch (IOException e) {
             e.printStackTrace();
         }
